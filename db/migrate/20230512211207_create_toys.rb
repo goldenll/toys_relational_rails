0cdc24ebd9f33baf6e17103d7dc802fa501d6982
Lauren Golden <llgolden@gmail.com>
2023-05-12 15:14:06 -0600

    Created databases and tables

---
+++ db/migrate/20230512211207_create_toys.rb
@@ -0,0 +1,12 @@
+class CreateToys < ActiveRecord::Migration[7.0]
+  def change
+    create_table :toys do |t|
+      t.string :name
+      t.integer :play_count
+      t.boolean :age_appropriate
+      t.references :box, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
