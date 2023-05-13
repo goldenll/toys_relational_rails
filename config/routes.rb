b0dedb5f94a7db711550309d015b7d5c54ccbc2c
Lauren Golden <llgolden@gmail.com>
2023-05-13 15:52:23 -0600

    Completed User Story 3

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   get '/boxes', to: 'boxes#index'
   get '/boxes/:id', to: 'boxes#show'
+  get '/toys', to: 'toys#index'
 end
--- spec/features/toys/index_spec.rb
+++ spec/features/toys/index_spec.rb
@@ -1,16 +1,24 @@
 require "rails_helper"
 
 RSpec.describe "/toys", type: :feature do 
+# User Story 3, Child Index
   describe "as a visitor, when I visit the toys index page" do
     it "I see each toy and its attributes" do
+      box1 = Box.create!(name: "Bedroom", capacity: 100, full: false)
+      toy1 = box1.toys.create!(name: "fire truck", play_count: 35, age_appropriate: true)
+      toy2 = Toy.create!(name: "red ball", play_count: 75, age_appropriate: true, box: box1)
+
       visit "/toys"
 
+      expect(page).to have_content("Toys")
       expect(page).to have_content(toy1.name)
       expect(page).to have_content(toy1.play_count)
       expect(page).to have_content(toy1.age_appropriate)
+
       expect(page).to have_content(toy2.name)
       expect(page).to have_content(toy2.play_count)
       expect(page).to have_content(toy2.age_appropriate)
+      # save_and_open_page
     end
   end
 end
