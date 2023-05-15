779a607d02c630400eca4af1e86206a2bf8e267d
Lauren Golden <llgolden@gmail.com>
2023-05-15 17:37:47 -0600

    US12 setup

---
+++ spec/features/boxes/update_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe "/boxes", type: :feature do 
+  describe "as a visitor, when I visit the boxes index page" do
+# User Story 12, Parent Update 
+# As a visitor
+# When I visit a parent show page
+# Then I see a link to update the parent "Update Parent"
+# When I click the link "Update Parent"
+# Then I am taken to '/parents/:id/edit' where I  see a form to edit the parent's attributes:
+# When I fill out the form with updated information
+# And I click the button to submit the form
+# Then a `PATCH` request is sent to '/parents/:id',
+# the parent's info is updated,
+# and I am redirected to the Parent's Show page where I see the parent's updated info
+
+    it "has a link to create a new Box record" do
+
+    end
+  end
+end
