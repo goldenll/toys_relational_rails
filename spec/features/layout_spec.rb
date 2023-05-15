c3ef9ac187e3d6ed4916a3ca8852384de69bee7e
Lauren Golden <llgolden@gmail.com>
2023-05-15 15:43:42 -0600

    Completed US 9

--- spec/features/layout_spec.rb
+++ spec/features/layout_spec.rb
@@ -24,8 +24,25 @@ RSpec.describe "any page on the site" do
       expect(page).to have_content("Toy Index Page")
     end
 # User Story 9
-    xit "has a link that takes me to the Box Index" do 
+    it "has a link that takes me to the Box Index" do 
+      box1 = Box.create!(name: "Bedroom", capacity: 100, full: false)
+      toy1 = box1.toys.create!(name: "fire truck", play_count: 35, age_appropriate: true)
+      toy2 = Toy.create!(name: "red ball", play_count: 75, age_appropriate: true, box: box1)
 
+      visit "/boxes"
+      expect(page).to have_content("Box Index Page")
+
+      visit "/boxes/#{box1.id}"
+      expect(page).to have_content("Box Index Page")
+
+      visit "/toys"
+      expect(page).to have_content("Box Index Page")
+
+      visit "/toys/#{toy1.id}"
+      expect(page).to have_content("Box Index Page")
+
+      visit "/boxes/#{box1.id}/toys"
+      expect(page).to have_content("Box Index Page")
     end
   end
 end
