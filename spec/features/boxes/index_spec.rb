f7c89c11a0858c459dbaa0b52e6a942479a0649a
Lauren Golden <llgolden@gmail.com>
2023-05-15 17:31:50 -0600

    Refactoring

--- spec/features/boxes/index_spec.rb
+++ spec/features/boxes/index_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "/boxes", type: :feature do 
-# User Story 1, Parent Index
   describe "as a visitor, when I visit the boxes index page" do
+  # User Story 1, Parent Index
     it "I see the name of each toy box" do
       box1 = Box.create!(name: "Bedroom", capacity: 100, full: false)
       box2 = Box.create!(name: "Living Room", capacity: 50, full: true)
--- spec/features/boxes/new_spec.rb
+++ spec/features/boxes/new_spec.rb
@@ -3,22 +3,13 @@ require "rails_helper"
 RSpec.describe "/boxes", type: :feature do 
   describe "as a visitor, when I visit the boxes index page" do
     # User Story 11, Parent Creation 
-    # Then I see a link to create a new Parent record, "New Parent"
-    # When I click this link
-    # Then I am taken to '/parents/new' where I  see a form for a new parent record
-    # When I fill out the form with a new parent's attributes:
-    # And I click the button "Create Parent" to submit the form
-    # Then a `POST` request is sent to the '/parents' route,
-    # a new parent record is created,
-    # and I am redirected to the Parent Index page where I see the new Parent displayed.
     it "has a link to create a new Box record" do
       visit "/boxes"
 
       click_link "New Box"
 
       expect(current_path).to eq("/boxes/new")
-      save_and_open_page
-      
+
       fill_in "Name", with: "Toy Room"
       fill_in "Capacity", with: "200"
       fill_in "Full?", with: "false"
